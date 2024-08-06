e9762dfd1cfa4901956ae4ee384237dae4478f86
blackbarman666 <mihaman05@gmail.com>
2024-08-06 18:08:35 +0300

    Add files via upload

---
+++ home work.py
@@ -0,0 +1,15 @@
+             #1st program
+print(9**0.5*5)
+              #2nd program
+print(9.99>9.98, 1000 !=1001)
+              #3rd program
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+             #4th program
+print(123.456)
+print(123.456*10)
+print(int(1234.56-1230.56))
+
+
+
